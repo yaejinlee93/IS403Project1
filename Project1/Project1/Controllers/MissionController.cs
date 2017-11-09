da97dcf3588bb638e70b5b9da3cfa29aa0edf407
justinwride <32500970+justinwride@users.noreply.github.com>
2017-11-09 14:30:48 -0700

    I added the MissionController, Missions View, and ViewMission View

---
+++ Project1/Project1/Controllers/MissionController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MissionSiteProject.Controllers
+{
+    public class MissionController : Controller
+    {
+        // GET: Mission
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Missions()
+        {
+            return View();
+        }
+
+        public ActionResult ViewMission(string mission)
+        {
+            ViewBag.missionName = mission;
+            if(mission == "Mission 1")
+            {
+                ViewBag.missionPresident = "";
+                ViewBag.missionAddress = "";
+                ViewBag.missionLanguage = "";
+                ViewBag.missionClimate = "";
+                ViewBag.missionReligion = "";
+                ViewBag.missionFlag = "";
+            }
+            else if(mission == "Mission 2")
+            {
+                ViewBag.missionPresident = "";
+                ViewBag.missionAddress = "";
+                ViewBag.missionLanguage = "";
+                ViewBag.missionClimate = "";
+                ViewBag.missionReligion = "";
+                ViewBag.missionFlag = "";
+            }
+            else if(mission == "Mission 3")
+            {
+                ViewBag.missionPresident = "";
+                ViewBag.missionAddress = "";
+                ViewBag.missionLanguage = "";
+                ViewBag.missionClimate = "";
+                ViewBag.missionReligion = "";
+                ViewBag.missionFlag = "";
+            }
+            return View();
+        }
+    }
+}
