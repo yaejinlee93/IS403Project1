2e72ef068809094c6d41a8ffb4473e27c7edb33b
justinwride <32500970+justinwride@users.noreply.github.com>
2017-12-12 15:44:49 -0700

    Added classes and stuff

--- Project1/Project1/Controllers/MissionController.cs
+++ Project1/Project1/Controllers/MissionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Project1.Models;
 
 namespace MissionSiteProject.Controllers
 {
---
+++ Project1/Project1/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project1.Models
+{
+    public class Users
+    {
+
+    }
+}
