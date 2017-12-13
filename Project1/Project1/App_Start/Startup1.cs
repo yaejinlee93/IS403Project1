0eedb6881d847a955dca74e9679ce7dabfd6e69c
justinwride <32500970+justinwride@users.noreply.github.com>
2017-12-12 17:59:18 -0700

    The Startup file doesn't work

--- Project1/Project1/App_Start/Startup1.cs
+++ Project1/Project1/App_Start/Startup1.cs
@@ -12,6 +12,27 @@ namespace Project1
     {
         public void Configuration(IAppBuilder app)
         {
+            // Configure the db context and user manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            // Configure the sign in cookie
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString(“/ Account / Login”),
+                Provider = new CookieAuthenticationProvider
+                {
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
             app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             {
                 ClientId = "755099474087 - eio039ptqh2sp8jfsn0eh1vgpmli082n.apps.googleusercontent.com",
